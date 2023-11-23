aebdc41f648cdb90e95067206deecb358e70c3e8
David Gold <davey3680@gmail.com>
2023-11-23 07:13:45 +0100

    profile setup

--- app/(general)/auth/page.js
+++ app/(general)/auth/page.js
@@ -4,7 +4,7 @@ import AuthCard from "@/app/components/AuthCard";
 
 export default function Signup() {
   return (
-    <main className="md:bg-bgImg bg-cover bg-no-repeat h-[calc(100vh-64px)] w-full">
+    <main className="md:bg-bgImg bg-cover bg-no-repeat h-[calc(100vh-64px)] w-full text-sm">
       <section className="h-full w-5/6 md:w-2/3 lg:w-1/2 xl:w-2/5 mx-auto flex justify-center md:items-center">
         <AuthCard/>
       </section>
