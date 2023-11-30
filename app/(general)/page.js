68b003523d81c0a79a96e7afbe76547d57ce8b97
David Gold <davey3680@gmail.com>
2023-11-30 20:58:20 +0100

    hero mobile

--- app/(general)/page.js
+++ app/(general)/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import BtnMain from "../components/BtnMain";
+import SearchBar from "../components/SearchBar";
 export default function Home() {
   return (
     <main>
-      <section className="grid grid-cols-12 grid-rows-[10] gap-x-3 mb-20">
-        <div className="row-start-[8] xl:row-start-[7] row-end-[11] col-start-[1] col-end-[3] w-4/5">
+      <section className="mt-5 mx-4 mb-20 flex gap-y-10 text-center md:text-left flex-wrap items-center md:gap-0 md:gap-x-3 md:mx-10 md:flex-nowrap lg:grid lg:grid-cols-12 lg:grid-rows-[10] lg:gap-x-3 lg:mx-0 lg:mt-0">
+        <div className="flex md:hidden justify-center w-full">
+          <SearchBar />
+        </div>
+        <div className="hidden lg:block row-start-[8] xl:row-start-[7] row-end-[11] col-start-[1] col-end-[3] w-4/5">
           <Image
             className="w-full"
             src={"/assets/plane.png"}
@@ -12,22 +16,22 @@ export default function Home() {
             height="100"
           />
         </div>
-        <div className="md:l-5 lg:pl-20 xl:pl-28 row-start-[2] row-end-[10] col-start-[1] col-end-[7] flex flex-col gap-6">
-          <h1 className="text-5xl leading-[3.5rem] font-extrabold">
+        <div className=" md:l-5 lg:pl-20 xl:pl-28 row-start-[2] row-end-[10] col-start-[1] col-end-[7] flex flex-col gap-4 md:gap-6">
+          <h1 className="text-3xl xl:text-5xl  xl:leading-[3.5rem] font-extrabold">
             Find Your <span className="text-tert">Travel Buddy</span> And Make
             Lifelong <span className="text-tert">Memories</span> 🏝️
           </h1>
-          <p className="text-lg">
+          <p className="text-base md:text-lg">
             Discover and connect with fellow travelers and create timeless
             journeys together
           </p>
-          <div className="flex items-center gap-x-5">
+          <div className="flex items-center gap-x-8 md:gap-x-8 justify-center md:justify-start">
             <BtnMain url={"/"}>
               Get Started
               <svg
                 className="rotate-180 stroke-white inline ml-2"
-                width="24"
-                height="24"
+                width="15"
+                height="18"
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +54,8 @@ export default function Home() {
                 />
               </svg>
             </BtnMain>
-            <div className="rounded-full w-10 h-10 p-2 flex justify-center items-center border-2 border-[#FBB040]">
+            <div className="flex items-center space-x-3">
+            <div className="shadow-xl rounded-full w-10 h-10 p-2 flex justify-center items-center border-[1px] border-[#FBB040]">
               <svg
                 className="ml-1"
                 width="14"
@@ -66,9 +71,10 @@ export default function Home() {
               </svg>
             </div>
             <span>Play Demo</span>
+            </div>
           </div>
         </div>
-        <div className="row-start-[2] row-end-[10] col-start-[7] col-end-[10]">
+        <div className="hidden lg:block row-start-[2] row-end-[10] col-start-[7] col-end-[10]">
           <div>
             <Image
               className="h-full w-full"
@@ -78,7 +84,7 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="row-start-[1] row-end-[3] col-start-[10] col-end-[13] mx-auto">
+        <div className="hidden lg:block row-start-[1] row-end-[3] col-start-[10] col-end-[13] lg:-mb-6 xl:-mb-3 mx-auto">
           <Image
             className=""
             src={"/assets/kite.png"}
@@ -86,7 +92,7 @@ export default function Home() {
             height="100"
           />
         </div>
-        <div  className="row-start-[3] row-end-[11] col-start-[10] col-end-[13]">
+        <div className="row-start-[3] row-end-[11] col-start-[10] col-end-[13] rounded-3xl overflow-clip">
           <Image
             className="w-full h-full"
             src={"/assets/heroBi.png"}
@@ -95,8 +101,9 @@ export default function Home() {
           />
         </div>
       </section>
-      <div className="flex justify-center mb-20">
-       </div>
+      <div className="hidden md:flex justify-center mb-20">
+        <SearchBar />
+      </div>
       <section>
         <div></div>
         <div></div>
---
+++ app/components/SearchBar.jsx
@@ -0,0 +1,33 @@
+import BtnMain from "./BtnMain"
+export default function SearchBar({dummyQuery="Where are you going to?"}) {
+  return (
+    <div style={{boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)'}} className="px-2 py-2 rounded-[50px] w-full md:w-3/5 lg:w-1/2 flex items-center justify-center gap-3 ">
+          <div className="ml-5">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11 20C15.9706 20 20 15.9706 20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20Z"
+                stroke="#FA8443"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M18.9299 20.6898C19.4599 22.2898 20.6699 22.4498 21.5999 21.0498C22.4499 19.7698 21.8899 18.7198 20.3499 18.7198C19.2099 18.7098 18.5699 19.5998 18.9299 20.6898Z"
+                stroke="#FA8443"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+          <input placeholder={dummyQuery} className="w-5/6 focus:outline-none"/>
+          <BtnMain>Search</BtnMain>
+        </div>
+  )
+}
