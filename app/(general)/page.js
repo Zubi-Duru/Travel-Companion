72106717cadcb065758e94d765c28afc63fb35d1
David Gold <davey3680@gmail.com>
2023-12-01 06:28:39 +0100

    fixed tab view of hero

--- app/(general)/page.js
+++ app/(general)/page.js
@@ -4,7 +4,7 @@ import SearchBar from "../components/SearchBar";
 export default function Home() {
   return (
     <main>
-      <section className="mt-8 mx-4 mb-20 flex gap-y-12 text-center md:text-left flex-wrap items-center md:gap-0 md:gap-x-3 md:mx-10 md:flex-nowrap lg:grid lg:grid-cols-12 lg:grid-rows-[10] lg:gap-x-3 lg:mx-0 lg:mt-0">
+      <section className="mt-8 mx-4 mb-20 flex gap-y-10 text-center md:text-left flex-wrap items-center md:gap-0 md:gap-x-3 md:mx-10 md:flex-nowrap lg:grid lg:grid-cols-12 lg:grid-rows-[10] lg:gap-x-3 lg:mx-0 lg:mt-0">
         <div className="flex md:hidden justify-center w-full">
           <SearchBar />
         </div>
@@ -16,7 +16,7 @@ export default function Home() {
             height="100"
           />
         </div>
-        <div className=" md:l-5 lg:pl-20 xl:pl-28 row-start-[2] row-end-[10] col-start-[1] col-end-[7] flex flex-col gap-5 md:gap-6">
+        <div className="md:basis-1/2 w-full md:l-5 lg:pl-20 xl:pl-28 row-start-[2] row-end-[10] col-start-[1] col-end-[7] flex flex-col gap-6">
           <h1 className=" text-3xl xl:text-5xl  xl:leading-[3.5rem] font-extrabold">
             Find Your <span className="text-tert">Travel Buddy</span> And Make
             Lifelong <span className="text-tert">Memories</span> 🏝️
@@ -55,22 +55,22 @@ export default function Home() {
               </svg>
             </BtnMain>
             <div className="flex items-center space-x-3">
-            <div className="shadow-xl rounded-full w-10 h-10 p-2 flex justify-center items-center border-[1px] border-[#FBB040]">
-              <svg
-                className="ml-1"
-                width="14"
-                height="14"
-                viewBox="0 0 14 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M13.1729 7.14401L0.841121 14.2637L0.841122 0.0242767L13.1729 7.14401Z"
-                  fill="#FBB040"
-                />
-              </svg>
-            </div>
-            <span>Play Demo</span>
+              <div className="shadow-xl rounded-full w-10 h-10 p-2 flex justify-center items-center border-[1px] border-[#FBB040]">
+                <svg
+                  className="ml-1"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.1729 7.14401L0.841121 14.2637L0.841122 0.0242767L13.1729 7.14401Z"
+                    fill="#FBB040"
+                  />
+                </svg>
+              </div>
+              <span>Play Demo</span>
             </div>
           </div>
         </div>
@@ -84,6 +84,7 @@ export default function Home() {
             />
           </div>
         </div>
+
         <div className="hidden lg:block row-start-[1] row-end-[3] col-start-[10] col-end-[13] lg:-mb-6 xl:-mb-3 mx-auto">
           <Image
             className=""
@@ -92,13 +93,23 @@ export default function Home() {
             height="100"
           />
         </div>
-        <div className="row-start-[3] row-end-[11] col-start-[10] col-end-[13] rounded-3xl overflow-clip">
-          <Image
-            className="w-full h-full"
-            src={"/assets/heroBi.png"}
-            width="1000"
-            height="1000"
-          />
+        <div className="md:basis-1/2 w-full row-start-[3] row-end-[11] col-start-[10] col-end-[13]">
+          <div className="inline-block md:hidden mx-auto -mb-10">
+            <Image
+              className=""
+              src={"/assets/kite.png"}
+              width="100"
+              height="100"
+            />
+          </div>
+          <div className="rounded-3xl overflow-clip">
+            <Image
+              className="w-full h-full"
+              src={"/assets/heroBi.png"}
+              width="1000"
+              height="1000"
+            />
+          </div>
         </div>
       </section>
       <div className="hidden md:flex justify-center mb-20">
--- app/components/BtnMain.jsx
+++ app/components/BtnMain.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 export default function BtnMain({url="",children}) {
   return (
-    <Link href={url} className="rounded-3xl text-white px-3 md:px-5 py-2 md:py-2.5 box-border bg-gradient-to-br border-[1px] border-transparent from-[#FD6E6A] to-[#FFC600] inline-block">
+    <Link href={url} className="rounded-3xl text-white px-3 font-semibold md:px-5 py-2 md:py-2.5 box-border bg-gradient-to-br border-[1px] border-transparent from-[#FD6E6A] to-[#FFC600] inline-block">
       {children}
     </Link>
   );
