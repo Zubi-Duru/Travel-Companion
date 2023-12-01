f3916890a5ce8b6a000fcfa9e4fd107567ba4cd4
David Gold <davey3680@gmail.com>
2023-12-01 08:34:46 +0100

    get started section with dummy img

--- app/components/Card1.jsx
+++ app/components/Card1.jsx
@@ -1,11 +1,11 @@
 export default function Card1({ svgIcon, title, content }) {
   return (
-    <div className="flex items-center gap-8 w-4/5">
-      <div className="bg-[#EFE084] flex items-center justify-center rounded-md p-2 ">
+    <div className="flex items-start gap-4 md:gap-8 xl:w-4/5">
+      <div className="bg-[#EFE084] mt-2 flex items-center justify-center rounded-md p-2 ">
         {svgIcon}
       </div>
       <div>
-        <h4 className="text-[#5E6282] text-lg mb-1 font-semibold">{title}</h4>
+        <h4 className="text-[#5E6282] text-base xl:text-lg md:mb-1 font-semibold">{title}</h4>
         <p className="text-sm">{content}</p>
       </div>
     </div>
