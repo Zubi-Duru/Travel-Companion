217016de1185eaac24ae07b11064cad993b0286a
David Gold <davey3680@gmail.com>
2023-12-14 20:59:08 +0100

    Redo hero sect1 pt1

--- app/(general)/page.js
+++ app/(general)/page.js
@@ -6,8 +6,11 @@ import Card1 from "../components/Card1";
 export default function Home() {
   return (
     <main>
-      <section className="flex items-center mb-20">
-        <div className="basis-1/2 relative lg:-mt-14">
+      <div className="md:hidden my-5 mx-6">
+        <SearchBar />
+      </div>
+      <section className="flex flex-col gap-10 md:flex-row md:gap-0 items-center mb-12 md:mb-20">
+        <div className="text-center md:text-start basis-1/2 relative lg:-mt-14">
           <div className="md:ml-12 lg:ml-20 xl:ml-36 space-y-5 relative z-10">
             <h1 className="text-3xl xl:text-[2.6rem]  xl:leading-[3.6rem] font-bold xl:font-extrabold">
               Find Your <span className="text-tert">Travel Buddy</span> And Make
@@ -66,7 +69,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="md:w-24 lg:w-36 xl:w-48 absolute md:-bottom-12 lg:-bottom-20 z-0">
+          <div className="hidden md:block md:w-24 lg:w-36 xl:w-48 absolute md:-bottom-12 lg:-bottom-20 z-0">
             <Image
               className="w-full h-full"
               src={"/assets/plane.png"}
@@ -75,7 +78,7 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="flex basis-1/2 items-center">
+        <div className="hidden md:flex basis-1/2 items-center">
           <div className="basis-1/2">
             <Image
               className="h-full w-full"
@@ -103,46 +106,48 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <div className="md:hidden">
+          <Image
+            className="w-full h-full"
+            src={"/assets/heroMob.png"}
+            width="500"
+            height="500"
+          />
+        </div>
       </section>
       <div className="hidden md:flex justify-center mb-20">
         <SearchBar />
       </div>
-      <section className="text-sm mx-4 md:px-5 lg:px-20 xl:px-36 lg:flex items-center">
+      <section className="text-sm mx-4 md:mx-12 lg:mx-20 xl:mx-36 lg:flex items-center">
         <div className="basis-1/2 w-full">
           <div className="mb-10">
-            <h2 className="text-tert text-lg font-semibold mb-2">QUICK GUIDE</h2>
+            <h2 className="text-tert text-lg font-semibold mb-2">
+              QUICK GUIDE
+            </h2>
             <h3 className="text-3xl xl:text-5xl xl:leading-[3.5rem] font-extrabold text-[#14183E]">
               How to get Started🤙🏻
             </h3>
           </div>
           <div className="space-y-6">
             <Card1
-              svgIcon={
-                <p className="text-xl">🔎</p>
-              }
+              svgIcon={<p className="text-xl">🔎</p>}
               title="Search for Destination"
               content=" for your destination at the top bar and you will be
               prompted to signup if you don&#39;t have an account"
             />
             <Card1
-              svgIcon={
-                <p className="text-xl">✏️</p>
-              }
+              svgIcon={<p className="text-xl">✏️</p>}
               title="Login/Sign up "
               content="Login/SIgn up to be able to explore our beautiful destinations and meet amazing travelers"
             />
             <Card1
-              svgIcon={
-                <p className="text-xl">😍</p>
-              }
+              svgIcon={<p className="text-xl">😍</p>}
               title="Explore & Connect With Like-Minded"
               content="You can join communities and connect with like minded travelers going towards your destination"
             />
           </div>
         </div>
-        <div className="basis-1/2 w-full hidden lg:block">
-
-        </div>
+        <div className="basis-1/2 w-full hidden lg:block"></div>
       </section>
       <section></section>
     </main>
