39645217bc3b6253a7ded2e1a891a69a0f0d958d
David Gold <davey3680@gmail.com>
2024-01-23 10:43:16 +0100

    chat engine custom

--- app/components/chat/DMChat.jsx
+++ app/components/chat/DMChat.jsx
@@ -1,17 +1,24 @@
 "use client";
 import React, { useState } from "react";
 import { useGetData } from "@/app/components/hooks/useFetchData";
-import { ChatEngine, getOrCreateChat } from "react-chat-engine";
+import {
+  ChatEngine,
+  getOrCreateChat,
+  NewChatForm,
+  ChatFeed,
+} from "react-chat-engine";
 import { useAuthContext } from "@/app/components/hooks/useAuthContext";
 
-const projectID=process.env.NEXT_PUBLIC_CHAT_PROJECT_ID
+const projectID = process.env.NEXT_PUBLIC_CHAT_PROJECT_ID;
 
 const DMChat = () => {
-  const { user,token, dispatch } = useAuthContext();
+  const { user, token, dispatch } = useAuthContext();
   const [username, setUsername] = useState("");
-  const { data: friends, error, isLoading } = useGetData(`/connect-friends`,{token:token});
-
-  console.log(friends);
+  const {
+    data: friends,
+    error,
+    isLoading,
+  } = useGetData(`/connect-friends`, { token: token });
 
   function createDirectChat(creds) {
     const friendList = friends.map(({ username }) => username);
@@ -26,26 +33,43 @@ const DMChat = () => {
 
   function renderChatForm(creds) {
     return (
-      <div className="m-2">
+      <div className="m-2 flex">
         <input
-          className="py-1 mx-1 rounded-2xl"
-          placeholder="Search for a user"
+          className="py-1 mx-1 rounded-2xl pl-2 border-2 outline-tert"
+          placeholder="Search for a friend"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        <button onClick={() => createDirectChat(creds)}>Create</button>
+        <button
+          className="font-medium text-tert border-[1px] border-tert py-1 px-4 md:py-2 md:px-4 rounded-3xl hover:bg-tert hover:text-white "
+          onClick={() => createDirectChat(creds)}
+        >
+          Search
+        </button>
+      </div>
+    );
+  }
+
+  function renderChatFeed(chatEngineProps) {
+    return (
+      <div>
+        <NewChatForm />
+        <ChatFeed {...chatEngineProps} />
       </div>
     );
   }
 
   return (
-    <ChatEngine
-      height="100vh"
-      userName={user?.username}
-      userSecret={user?._id}
-      projectID={projectID}
-      renderNewChatForm={(creds) => renderChatForm(creds)}
-    />
+    <div>
+      <ChatEngine
+        height="100vh"
+        userName={user?.username}
+        userSecret={user?._id}
+        projectID={projectID}
+        renderNewChatForm={(creds) => renderChatForm(creds)}
+        renderChatFeed={(chatEngineProps) => renderChatFeed(chatEngineProps)}
+      />
+    </div>
   );
 };
 
